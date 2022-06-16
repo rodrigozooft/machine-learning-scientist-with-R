871bdbc49e8194547ca07ce38a7c83dc613abfea
Rod Morgado <rodrigo@zooft.company>
2022-06-16 16:38:13 -0700

    Hey, that's a god looking plot.

--- fundamentals-of-bayesian-data-analysis-in-r.R
+++ fundamentals-of-bayesian-data-analysis-in-r.R
@@ -4,3 +4,13 @@ prop_model(data)
 # Update the data and rerun prop_model
 data = c(1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0)
 prop_model(data)
+
+data = c(1, 0, 0, 1, 0, 0,
+         0, 0, 0, 0, 0, 0, 0)
+
+# Extract and explore the posterior
+posterior <- prop_model(data)
+head(posterior)
+
+# Edit the histogram
+hist(posterior, breaks = 30, xlim = c(0, 1), col = 'palegreen4')
