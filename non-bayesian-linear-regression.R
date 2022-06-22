bac8af3b90bf9b367daadc859a620180deb3d802
Rod Morgado <rodrigo@zooft.company>
2022-06-21 21:14:25 -0700

    You've created a frequentist linear model and learned how to examine the output.

--- non-bayesian-linear-regression.R
+++ non-bayesian-linear-regression.R
@@ -3,3 +3,12 @@ head(songs, 6)
 
 # Print the structure
 str(songs)
+
+# Create the model here
+lm_model <- lm(popularity ~ song_age, data = songs)
+
+# Produce the summary
+summary(lm_model)
+
+# Print a tidy summary of the coefficients
+tidy(lm_model)
