af4a18f5f2b670b42dac51de904cf6986b9e5946
Rod Morgado <rodrigo@zooft.company>
2022-06-21 21:27:43 -0700

    You are well on your way to becoming a Bayeasian.

--- non-bayesian-linear-regression.R
+++ non-bayesian-linear-regression.R
@@ -21,3 +21,12 @@ summary(stan_model)
 
 # Print a tidy summary of the coefficients
 tidy(stan_model)
+
+# Create the 90% credible intervals
+posterior_interval(stan_model)
+
+# Create the 95% credible intervals
+posterior_interval(stan_model, prob = 0.95)
+
+# Create the 80% credible intervals
+posterior_interval(stan_model, prob = 0.8)
