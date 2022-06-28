a9d5bb4532f42c3cd6c4ce35b213faa34c86940f
Rod Morgado <rodrigo@zooft.company>
2022-06-28 13:10:43 -0700

    Slam dunk! ft_bucketizer() converts from continuous to categorical; now you'll see a common, special case of this.

--- introduction-to-spark-with-sparklyr.R
+++ introduction-to-spark-with-sparklyr.R
@@ -258,3 +258,24 @@ hotttnesss <- track_metadata_tbl %>%
 # Draw a barplot of is_hottt_or_nottt
 ggplot(hotttnesss, aes(is_hottt_or_nottt)) +
   geom_bar()
+
+# track_metadata_tbl, decades, decade_labels have been pre-defined
+track_metadata_tbl
+decades
+decade_labels
+
+hotttnesss_over_time <- track_metadata_tbl %>%
+  # Select artist_hotttnesss and year
+  select(artist_hotttnesss, year) %>%
+  # Convert year to numeric
+  mutate(year = as.numeric(year)) %>%
+  # Bucketize year to decade using decades vector
+  ft_bucketizer("year", "decade", splits = decades) %>%
+  # Collect the result
+  collect() %>%
+  # Convert decade to factor using decade_labels
+  mutate(decade = factor(decade, labels = decade_labels))
+
+# Draw a boxplot of artist_hotttnesss by decade
+ggplot(hotttnesss_over_time, aes(decade, artist_hotttnesss)) +
+  geom_boxplot()  
