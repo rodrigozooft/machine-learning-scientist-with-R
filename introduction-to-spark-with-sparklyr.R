bf01e0be2ad2da5f45253334435d5d9f725d53a9
Rod Morgado <rodrigo@zooft.company>
2022-06-28 13:00:00 -0700

    Hot stuff, ft_binarizer() converts from continuous to logical; now you'll see how to convert from continuous to categorical.

--- introduction-to-spark-with-sparklyr.R
+++ introduction-to-spark-with-sparklyr.R
@@ -241,3 +241,20 @@ joined <- semi_join(track_metadata_tbl, artist_terms_tbl, by = "artist_id")
 
 # How many rows and columns are in the joined table?
 dim(joined)
+
+# track_metadata_tbl has been pre-defined
+track_metadata_tbl
+
+hotttnesss <- track_metadata_tbl %>%
+  # Select artist_hotttnesss
+  select(artist_hotttnesss) %>%
+  # Binarize to is_hottt_or_nottt
+  ft_binarizer("artist_hotttnesss", "is_hottt_or_nottt", threshold = 0.5) %>%
+  # Collect the result
+  collect() %>%
+  # Convert is_hottt_or_nottt to logical
+  mutate(is_hottt_or_nottt = as.logical(is_hottt_or_nottt))
+
+# Draw a barplot of is_hottt_or_nottt
+ggplot(hotttnesss, aes(is_hottt_or_nottt)) +
+  geom_bar()
