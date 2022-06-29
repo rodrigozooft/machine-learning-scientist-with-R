674c46c863bc37a8274a5f34b1b9df0fd4401ccf
Rod Morgado <rodrigo@zooft.company>
2022-06-29 10:57:23 -0700

    You can escape quotes inside strings using a backslash.

---
+++ working-with-stringr.R
@@ -0,0 +1,8 @@
+# Define line1
+line1 <- "The table was a large one, but the three were all crowded together at one corner of it:"
+
+# Define line2
+line2 <- '"No room! No room!" they cried out when they saw Alice coming.'
+
+# Define line3
+line3 <- "\"There's plenty of room!\" said Alice indignantly, and she sat down in a large arm-chair at one end of the table."
