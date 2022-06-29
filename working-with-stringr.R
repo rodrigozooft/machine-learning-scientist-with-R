4c67a3d7171fcec55dc7e5cc2bfb3375214dc042
Rod Morgado <rodrigo@zooft.company>
2022-06-29 13:22:30 -0700

    If you wanted the dollar signs right next to the numbers, you could format the incomes with trim = TRUE.

--- working-with-stringr.R
+++ working-with-stringr.R
@@ -105,3 +105,21 @@ year_percent <- paste(years, ": ", pretty_percent, "%", sep = "")
 
 # Collapse all years into single string
 paste(year_percent, collapse = ", ")
+
+# Define the names vector
+income_names <- c("Year 0", "Year 1", "Year 2", "Project Lifetime")
+
+# Create pretty_income
+pretty_income <- format(income, digits = 2, big.mark = ",")
+
+# Create dollar_income
+dollar_income <- paste("$", pretty_income, sep = "")
+
+# Create formatted_names
+formatted_names <- format(income_names, justify = "right")
+
+# Create rows
+rows <- paste(formatted_names, dollar_income, sep = "   ")
+
+# Write rows
+writeLines(rows)
