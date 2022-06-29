b3bd095e0b1db21781454c8882fafa089de018b2
Rod Morgado <rodrigo@zooft.company>
2022-06-29 11:03:53 -0700

    The function cat() is very similar to writeLines(), but by default separates elements with a space.

--- working-with-stringr.R
+++ working-with-stringr.R
@@ -6,3 +6,18 @@ line2 <- '"No room! No room!" they cried out when they saw Alice coming.'
 
 # Define line3
 line3 <- "\"There's plenty of room!\" said Alice indignantly, and she sat down in a large arm-chair at one end of the table."
+
+# Putting lines in a vector
+lines <- c(line1, line2, line3)
+
+# Print lines
+print(lines)
+
+# Use writeLines() on lines
+writeLines(lines)
+
+# Write lines with a space separator
+writeLines(lines, sep = " ")
+
+# Use writeLines() on the string "hello\n\U1F30D"
+writeLines("hello\n\U1F30D")
