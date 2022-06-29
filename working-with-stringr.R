f6f8f094736237bf066e5558521c869eb77f6454
Rod Morgado <rodrigo@zooft.company>
2022-06-29 12:58:18 -0700

    Phenomenal work! format() is powerful but tricky to use. An alternative is provided by format()

--- working-with-stringr.R
+++ working-with-stringr.R
@@ -54,3 +54,23 @@ format(income, digits = 1, scientific = FALSE)
 
 # Format p_values in fixed format
 format(p_values, scientific = FALSE) 
+
+formatted_income <- format(income, digits = 2)
+
+# Print formatted_income
+print(formatted_income)
+
+# Call writeLines() on the formatted income
+writeLines(formatted_income)
+
+# Define trimmed_income
+trimmed_income <- format(income, digits = 2, trim = TRUE)
+
+# Call writeLines() on the trimmed_income
+writeLines(trimmed_income)
+
+# Define pretty_income
+pretty_income <- format(income, digits = 2, big.mark = ",")
+
+# Call writeLines() on the pretty_income
+writeLines(pretty_income)
