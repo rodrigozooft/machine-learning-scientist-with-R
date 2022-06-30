05c242b7486776f6209e080f9a13080ec8f37018
Rod Morgado <rodrigo@zooft.company>
2022-06-29 22:13:29 -0700

    Challenge completed. Now you'll learn how to specify patterns in ttext, using regex.

--- working-with-stringr.R
+++ working-with-stringr.R
@@ -324,3 +324,30 @@ str_subset(genes, pattern = fixed("TTTTTT"))
 
 # Replace all the "A"s in the sequences with a "_"
 str_replace_all(genes, pattern = fixed("A"), replacement = "_")
+
+# Define some full names
+names <- c("Diana Prince", "Clark Kent")
+
+# Split into first and last names
+names_split <- str_split(names, pattern = fixed(" "), simplify = TRUE)
+
+# Extract the first letter in the first name
+abb_first <- str_sub(names_split[, 1], 1, 1)
+
+# Combine the first letter ". " and last name
+str_c(abb_first, ". ", names_split[, 2])
+
+# Use all names in babynames_2014
+all_names <- babynames_2014$name
+
+# Get the last two letters of all_names
+last_two_letters <- str_sub(all_names, -2, -1)
+
+# Does the name end in "ee"?
+ends_in_ee <- str_detect(last_two_letters, pattern = "ee")
+
+# Extract rows and "sex" column
+sex <- subset(babynames_2014$sex, ends_in_ee)
+
+# Display result as a table
+table(sex)
