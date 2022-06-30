8f121a9c3c516d399c8905fb8ec1310c89a8b4c0
Rod Morgado <rodrigo@zooft.company>
2022-06-29 21:03:00 -0700

    Did you see that "A" is the most popular first letter for both boys and girls, and the most popular lasst letter for girls.

--- working-with-stringr.R
+++ working-with-stringr.R
@@ -189,3 +189,23 @@ mean(girl_length) - mean(boy_length)
 
 # Confirm str_length() works with factors
 head(str_length(factor(boy_names)))
+
+
+# Extract first letter from boy_names
+boy_first_letter <- str_sub(boy_names, 1, 1)
+
+# Tabulate occurrences of boy_first_letter
+table(boy_first_letter)
+  
+# Extract the last letter in boy_names, then tabulate
+boy_last_letter <- str_sub(boy_names, -1, -1)
+table(boy_last_letter)
+
+# Extract the first letter in girl_names, then tabulate
+girl_first_letter <- str_sub(girl_names, 1, 1)
+table(girl_first_letter)
+
+# Extract the last letter in girl_names, then tabulate
+girl_last_letter <- str_sub(girl_names, -1, -1)
+table(girl_last_letter)
+  
