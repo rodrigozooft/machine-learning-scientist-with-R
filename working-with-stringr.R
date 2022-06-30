afa30533a7facfca26353a1a93f2b2b38daf92a2
Rod Morgado <rodrigo@zooft.company>
2022-06-29 21:24:25 -0700

    Excellent work! I'm rolling on this.

--- working-with-stringr.R
+++ working-with-stringr.R
@@ -236,3 +236,20 @@ starts_U
 
 # Find girl_names that contain "U" and "z"
 str_subset(starts_U, pattern = fixed("z"))
+
+# Count occurrences of "a" in girl_names
+number_as <- str_count(girl_names, pattern = fixed("a"))
+
+# Count occurrences of "A" in girl_names
+number_As <- str_count(girl_names, pattern = fixed("A"))
+
+# Histograms of number_as and number_As
+hist(number_as)
+hist(number_As)  
+
+# Find total "a" + "A"
+total_as <- number_as + number_As
+
+
+# girl_names with more than 4 a's
+subset(girl_names, total_as > 4)
