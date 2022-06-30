ccd1946f3b3da41e0ecbf158e7556ca5871303e4
Rod Morgado <rodrigo@zooft.company>
2022-06-29 21:18:21 -0700

    Nice work! Only one girls' name that starts with "U" and contains a "z". Have you ever met an "Umaiza"?

--- working-with-stringr.R
+++ working-with-stringr.R
@@ -223,3 +223,16 @@ boy_names[contains_zz]
 
 # Which rows in boy_df have names that contain "zz"?
 subset(boy_df, contains_zz)
+
+# Find boy_names that contain "zz"
+str_subset(boy_names, pattern = fixed("zz"))
+
+# Find girl_names that contain "zz"
+str_subset(girl_names, pattern = fixed("zz"))
+
+# Find girl_names that contain "U"
+starts_U <- str_subset(girl_names, pattern = fixed("U"))
+starts_U
+
+# Find girl_names that contain "U" and "z"
+str_subset(starts_U, pattern = fixed("z"))
