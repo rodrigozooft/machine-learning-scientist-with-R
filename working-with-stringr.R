49cddbb9d60397146c56b36ed93a98d5cad320a5
Rod Morgado <rodrigo@zooft.company>
2022-06-29 17:43:55 -0700

    This behavior is nice because you learn quickly when youu might have missing values.

--- working-with-stringr.R
+++ working-with-stringr.R
@@ -141,3 +141,23 @@ my_order <- paste("I want to order a pizza with ", these_toppings, ".", sep = ""
 
 # Order pizza with writeLines()
 writeLines(my_order)
+
+library(stringr)
+
+my_toppings <- c("cheese", NA, NA)
+my_toppings_and <- paste(c("", "", "and "), my_toppings, sep = "")
+
+# Print my_toppings_and
+print(my_toppings_and)
+
+# Use str_c() instead of paste(): my_toppings_str
+my_toppings_str <- str_c(c("", "", "and "), my_toppings)
+
+# Print my_toppings_str
+print(my_toppings_str)
+
+# paste() my_toppings_and with collapse = ", "
+paste(my_toppings_and, collapse = ", ")
+
+# str_c() my_toppings_str with collapse = ", "
+str_c(my_toppings_str, collapse = ", ")
