b5546e985ef19b63bd71f87fabe16a35c782734e
Rod Morgado <rodrigo@zooft.company>
2022-06-29 21:53:10 -0700

    If you need to replace multiple characters you could use str_replace_all() more than once, but you'll see a better way.

--- working-with-stringr.R
+++ working-with-stringr.R
@@ -300,3 +300,15 @@ word_lengths <- lapply(words, str_length)
   
 # Average word length per line
 lapply(word_lengths, mean)
+
+# Some (fake) phone numbers
+phone_numbers <- c("510-555-0123", "541-555-0167")
+
+# Use str_replace() to replace "-" with " "
+str_replace(phone_numbers, pattern = fixed("-"), replacement = " ")
+
+# Use str_replace_all() to replace "-" with " "
+str_replace_all(phone_numbers, pattern = fixed("-"), replacement = " ")
+
+# Turn phone numbers into the format xxx.xxx.xxxx
+str_replace_all(phone_numbers, pattern = fixed("-"), replacement = ".")
