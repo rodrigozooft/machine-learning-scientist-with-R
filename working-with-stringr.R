50d5f1b44b651b6093bd030c2fafe30d39d3d3eb
Rod Morgado <rodrigo@zooft.company>
2022-07-01 11:32:39 -0700

    We'll see an easyy way to extract parts of our match (the numbers for example) in the next chapter.

--- working-with-stringr.R
+++ working-with-stringr.R
@@ -463,3 +463,42 @@ not_vowels <- negated_char_class("aeiouAEIOU")
 str_view(boy_names, 
   pattern = exactly(one_or_more(not_vowels)), 
   match = TRUE)
+
+# Create a separator pattern
+separator <- char_class("-.() ")
+
+# Test it
+str_view_all(contact, pattern = separator)
+
+# Use these components
+three_digits <- DGT %R% DGT %R% DGT
+four_digits <- three_digits %R% DGT
+separator <- char_class("-.() ")
+
+# Create phone pattern
+phone_pattern <- zero_or_more(OPEN_PAREN) %R%
+  three_digits %R%
+  zero_or_more(separator) %R%
+  three_digits %R% 
+  zero_or_more(separator) %R%
+  four_digits
+        
+# Test it           
+str_view_all(contact, pattern = phone_pattern)
+
+# Use this pattern
+three_digits <- DGT %R% DGT %R% DGT
+four_digits <- three_digits %R% DGT
+separator <- char_class("-.() ")
+phone_pattern <- optional(OPEN_PAREN) %R% 
+  three_digits %R% 
+  zero_or_more(separator) %R% 
+  three_digits %R% 
+  zero_or_more(separator) %R%
+  four_digits
+  
+# Extract phone numbers
+str_extract(contact, pattern = phone_pattern)
+
+# Extract ALL phone numbers
+str_extract_all(contact, pattern = phone_pattern)
