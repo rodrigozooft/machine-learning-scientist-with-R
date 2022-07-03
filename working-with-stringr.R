69bb70c3de97f2666bade464bc4baf9c7a8eee4c
Rod Morgado <rodrigo@zooft.company>
2022-07-03 13:09:27 -0700

    Your code is so sharp you might cut something! Replacement combined with backreferences.

--- working-with-stringr.R
+++ working-with-stringr.R
@@ -635,3 +635,19 @@ str_replace_all(contact, DGT, "X")
 
 # Replace all digits with different symbol
 str_replace_all(contact, DGT, c("X", ".", "*", "_"))
+
+# Build pattern to match words ending in "ING"
+pattern <- one_or_more(WRD) %R% "ING"
+str_view(narratives, pattern)
+
+# Test replacement
+str_replace(narratives, capture(pattern), 
+  str_c("CARELESSLY", REF1, sep = " "))
+
+# One adverb per narrative
+adverbs_10 <- sample(adverbs, 10)
+
+# Replace "***ing" with "adverb ***ly"
+str_replace(narratives, 
+  capture(pattern),
+  str_c(adverbs_10, REF1, sep = " "))  
