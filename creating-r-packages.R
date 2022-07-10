4d89b2d6ef3e3b4c4312f3d10b14ad35662fc46a
Rod Morgado <rodrigo@zooft.company>
2022-07-10 16:29:03 -0700

    Read to go

--- creating-r-packages.R
+++ creating-r-packages.R
@@ -3,3 +3,21 @@ create('datasummary')
 
 # Take a look at the files and folders in your package
 dir('datasummary')
+
+# Create numeric_summary() function
+numeric_summary <- function(x, na.rm) {
+
+    # Include an error if x is not numeric
+    if(!is.numeric(x)){
+        stop("Data must be numeric")
+    }
+    
+    # Create data frame
+    data.frame( min = min(x, na.rm = na.rm),
+                median = median(x, na.rm = na.rm),
+                sd = sd(x, na.rm = na.rm),
+                max = max(x, na.rm = na.rm))
+}
+
+# Test numeric_summary() function
+numeric_summary(airquality$Ozone, na.rm = TRUE)
