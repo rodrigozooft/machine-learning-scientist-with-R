2cd20aa8431a2cefd146c2a60e1d7ce73b7e0482
Rod Morgado <rodrigo@zooft.company>
2022-07-11 18:27:06 -0700

    Next we're going to look at additional documentation.

--- creating-r-packages.R
+++ creating-r-packages.R
@@ -144,3 +144,36 @@ data_summary <- function(x, na.rm = TRUE){
   map_df(num_data, .f = numeric_summary, na.rm = na.rm, .id = "ID")
   
 }
+
+#' Data Summary for Numeric Columns
+#'
+#' Custom summaries of numeric data in a provided data frame
+#'
+#' @param x A data.frame containing at least one numeric column
+#' @param na.rm A logical indicating whether missing values should be removed
+#' @import dplyr
+#' @import purrr
+#' @importFrom tidyr gather
+#' @export
+#' @examples
+#' data_summary(iris)
+#' data_summary(airquality, na.rm = FALSE)
+#'
+## Update the details for the return value
+#' @return This function returns a \code{data.frame} including columns:
+#' \itemize{
+#'  \item ID
+#'  \item min
+#'  \item median
+#'  \item sd
+#'  \item max
+#' }
+#'
+#' @export
+data_summary <- function(x, na.rm = TRUE){
+  
+  num_data <- select_if(x, .predicate = is.numeric) 
+  
+  map_df(num_data, .f = numeric_summary, na.rm = na.rm, .id = "ID")
+  
+}
