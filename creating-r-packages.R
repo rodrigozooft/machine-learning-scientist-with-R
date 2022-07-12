7798ece26f0334068622a985500a1966567c4fcc
Rod Morgado <rodrigo@zooft.company>
2022-07-11 18:45:28 -0700

    Now that you know how to document the functions in a package.

--- creating-r-packages.R
+++ creating-r-packages.R
@@ -177,3 +177,37 @@ data_summary <- function(x, na.rm = TRUE){
   map_df(num_data, .f = numeric_summary, na.rm = na.rm, .id = "ID")
   
 }
+
+#' Summary of Numeric Columns
+#' Generate specific summaries of numeric columns in a data frame
+#'
+#' @param x A data frame. Non-numeric columns will be removed
+#' @param na.rm A logical indicating whether missing values should be removed
+#' @import dplyr
+#' @import purrr
+#' @importFrom tidyr gather
+#' @export
+#' @examples
+#' data_summary(iris)
+#' data_summary(airquality, na.rm = FALSE)
+#' 
+#' @return This function returns a \code{data.frame} including columns: 
+#' \itemize{
+#'  \item ID
+#'  \item min
+#'  \item median
+#'  \item sd
+#'  \item max
+#' }
+#'
+## Add in the author of the `data_summary()` function
+#' @author My Name <myemail@example.com>
+## List the `summary()` function (from the `base` package)
+#' @seealso \link[base]{summary}
+data_summary <- function(x, na.rm = TRUE){
+  
+  num_data <- select_if(x, .predicate = is.numeric) 
+  
+  map_df(num_data, .f = numeric_summary, na.rm = na.rm, .id = "ID")
+  
+}
