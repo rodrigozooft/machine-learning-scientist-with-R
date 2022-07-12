be57e5f5b9558b75de97fa1f79258326181b1c13
Rod Morgado <rodrigo@zooft.company>
2022-07-12 11:11:12 -0700

    Using expect as unit test function to check the result from a function.

--- creating-r-packages.R
+++ creating-r-packages.R
@@ -299,3 +299,12 @@ dir("datasummary")
 
 # Look at the contents of the new folder which has been created 
 dir("datasummary/tests")
+
+# Create a summary of the iris dataset using your data_summary() function
+iris_summary <- data_summary(iris)
+
+# Count how many rows are returned
+summary_rows <- nrow(iris_summary) 
+
+# Use expect_equal to test that calling data_summary() on iris returns 4 rows
+expect_equal(summary_rows, 4)
